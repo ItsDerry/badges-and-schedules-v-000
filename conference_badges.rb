08aec788563605ee53f16cd1f994c1def5ca6d52
Derrick Reyes <derrickreyes@me.com>
2018-08-21 18:27:34 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,7 +10,13 @@ def batch_badge_creator(name)
   badges
 end
 
-def assign_rooms
+def assign_rooms(name, room_assignments)
+    assigned_rooms = Array.new
+    name.each do |attendee, room|
+    assigned_rooms.push("Hello, #{attendee}! You'll be assigned to room #{room}!")
+    end
+    badges
+  end
 end
 
 def printer
