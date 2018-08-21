35d963af479d64520e9605caf7647dec86264f52
Derrick Reyes <derrickreyes@me.com>
2018-08-21 19:46:10 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -19,7 +19,7 @@ def assign_rooms(attendees)
     assigned_rooms
 end
 
-def printer(name, attendees)
+def printer(attendees)
   badges = Array.new
   name.each do |attendee|
   badges.push("Hello, my name is #{attendee}.")
