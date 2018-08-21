b1de185e5679f94208376e94d2ac3d7440559700
Derrick Reyes <derrickreyes@me.com>
2018-08-21 18:24:22 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,7 +5,7 @@ end
 def batch_badge_creator(name)
   badges = Array.new
   name.each do |attendee|
-  badges.push("Hello, my name is #{name}.")
+  badges.push("Hello, my name is #{attendee}.")
   end
   badges
 end
