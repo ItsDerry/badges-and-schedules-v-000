e9f54a0e6c1ed01c38a029fe1077bf657bd23e0f
Derrick Reyes <derrickreyes@me.com>
2018-08-21 19:05:15 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,10 +10,10 @@ def batch_badge_creator(name)
   badges
 end
 
-def assign_rooms(attendees, room_assignments)
+def assign_rooms(attendees, index)
     assigned_rooms = Array.new
-    name.each_with_index do |attendee, room|
-    assigned_rooms.push("Hello, #{attendee}! You'll be assigned to room #{room}!")
+    name.each_with_index do |attendee, index|
+    assigned_rooms.push("Hello, #{attendee}! You'll be assigned to room #{index}!")
     end
     assigned_rooms
 end
